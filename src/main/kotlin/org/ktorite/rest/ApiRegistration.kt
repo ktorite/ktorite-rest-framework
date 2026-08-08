24698f23828268e78507f7a92d9ead8eef92352b
rajeev-k47 <rajeevkumawat2007@outlook.com>
2026-08-09 03:55:05 +0530

    <Feat: Adds middleware to perform task before and after api call execution>?

--- src/main/kotlin/org/ktorite/rest/ApiRegistration.kt
+++ src/main/kotlin/org/ktorite/rest/ApiRegistration.kt
@@ -1,5 +1,7 @@
 package org.ktorite.rest
 
+import io.ktor.server.application.*
+import kotlinx.serialization.json.*
 import org.jetbrains.exposed.v1.core.Table
 
 data class ApiRegistration(
@@ -8,5 +10,11 @@ data class ApiRegistration(
     var paginated: Boolean = true,
     var path: String = table.tableName.lowercase(),
     var pageSize: Int = 20,
-    var maxPageSize: Int = 100
+    var maxPageSize: Int = 100,
+    var beforeCreate: (suspend (ApplicationCall, JsonObject) -> JsonObject?)? = null,
+    var afterCreate: (suspend (ApplicationCall, JsonArray) -> Unit)? = null,
+    var beforeUpdate: (suspend (ApplicationCall, String, JsonObject) -> JsonObject?)? = null,
+    var afterUpdate: (suspend (ApplicationCall, String) -> Unit)? = null,
+    var beforeDelete: (suspend (ApplicationCall, String) -> Boolean)? = null,
+    var afterDelete: (suspend (ApplicationCall, String) -> Unit)? = null
 )
